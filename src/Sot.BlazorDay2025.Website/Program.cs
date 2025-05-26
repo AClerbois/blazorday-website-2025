b8a34aee5cba896ea97234ca4625ac386409b330
Denis Voituron <dvoituron@microsoft.com>
2025-05-26 10:42:06 +0200

    Add FluentUI Blazor and remove Boostrap

--- src/Sot.BlazorDay2025.Website/Program.cs
+++ src/Sot.BlazorDay2025.Website/Program.cs
@@ -4,11 +4,15 @@
 
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.FluentUI.AspNetCore.Components;
 using Sot.BlazorDay2025.Website;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+// Register Fluent UI services
+builder.Services.AddFluentUIComponents();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
