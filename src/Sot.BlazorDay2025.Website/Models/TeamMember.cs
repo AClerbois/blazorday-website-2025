537285edf5921b77e3692ab1d5659101afb67a3e
Denis Voituron <dvoituron@outlook.com>
2025-05-30 10:51:47 +0200

    Add Accessibility et refactoring

---
+++ src/Sot.BlazorDay2025.Website/Models/TeamMember.cs
@@ -0,0 +1,21 @@
+// ------------------------------------------------------------------------
+// MIT License - Copyright (c) BlazorDay 2025. All rights reserved.
+// ------------------------------------------------------------------------
+
+namespace Sot.BlazorDay2025.Website.Models;
+
+/// <summary />
+public class TeamMember
+{
+    /// <summary />
+    public required string Name { get; set; }
+
+    /// <summary />
+    public required string Title { get; set; }
+
+    /// <summary />
+    public required string ImageUrl { get; set; }
+
+    /// <summary />
+    public required string LinkedIn { get; set; }
+}
--- src/Sot.BlazorDay2025.Website/Pages/TeamPage.razor.cs
+++ src/Sot.BlazorDay2025.Website/Pages/TeamPage.razor.cs
@@ -2,9 +2,60 @@
 // MIT License - Copyright (c) BlazorDay 2025. All rights reserved.
 // ------------------------------------------------------------------------
 
+using Microsoft.AspNetCore.Components;
+using Microsoft.FluentUI.AspNetCore.Components;
+using Microsoft.JSInterop;
+using Sot.BlazorDay2025.Website.Models;
+
 namespace Sot.BlazorDay2025.Website.Pages;
 
 /// <summary />
 public partial class TeamPage
 {
+    private static readonly Icon OpenIcon = new Microsoft.FluentUI.AspNetCore.Components.Icons.Regular.Size20.Open();
+
+    /// <summary />
+    private static readonly TeamMember[] TeamMembers =
+    [
+        new TeamMember
+        {
+            Name = "Laurent Bugnion",
+            Title = "Principal Cloud Developer Advocate @ Microsoft",
+            ImageUrl = "/img/team-laurent.jpg",
+            LinkedIn = "https://www.linkedin.com/in/lbugnion/",
+        },
+         new TeamMember
+        {
+            Name = "Adrien Clerbois",
+            Title = "Microsoft MVP & .NET Technical Architect",
+            ImageUrl = "/img/team-adrien.jpg",
+            LinkedIn = "https://www.linkedin.com/in/aclerbois/",
+        },
+         new TeamMember
+        {
+            Name = "Christophe Peugnet",
+            Title = "Microsoft MVP & .NET / Blazor Dev | Formateur",
+            ImageUrl = "/img/team-christophe.jpg",
+            LinkedIn = "https://www.linkedin.com/in/christophepeugnet/",
+        },
+         new TeamMember
+        {
+            Name = " Denis Voituron",
+            Title = "Principal Software Engineer @ Microsoft",
+            ImageUrl = "/img/team-denis.jpg",
+            LinkedIn = "https://www.linkedin.com/in/denisvoituron/",
+        },
+    ];
+
+    /// <summary />
+    [Inject]
+    public required IJSRuntime JSRuntime { get; set; }
+
+    /// <summary>
+    /// Display the speaker detail page
+    /// </summary>
+    private async Task OpenTeamDetailAsync(string url)
+    {
+        await JSRuntime.InvokeVoidAsync("openInNewTab", url);
+    }
 }
